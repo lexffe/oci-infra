3fdc4902a8a97a7a3ed4659661629d1dd709f8bc
lexffe <lexffe@users.noreply.github.com>
2021-12-31 16:11:57 +0000

    add cf state file

--- objects.tf
+++ objects.tf
@@ -8,3 +8,20 @@ resource "oci_objectstorage_preauthrequest" "this" {
   namespace    = data.oci_objectstorage_namespace.ns.namespace
   time_expires = "2022-06-30T23:59:59.000Z"
 }
+
+resource "oci_objectstorage_object" "cf_state" {
+  bucket = oci_objectstorage_bucket.state.name
+  namespace = data.oci_objectstorage_namespace.ns.namespace
+  object = "cf-infra.tfstate"
+
+  content = "" # content managed by external world
+}
+
+resource "oci_objectstorage_preauthrequest" "cf_state" {
+  object_name  = oci_objectstorage_object.cf_state.object
+  access_type  = "ObjectReadWrite"
+  bucket       = oci_objectstorage_bucket.state.name
+  name         = "bootstrap"
+  namespace    = data.oci_objectstorage_namespace.ns.namespace
+  time_expires = "2022-06-30T23:59:59.000Z"
+}
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "oci_state_par" {
   description = "State file PAR URI for this repository"
   value = oci_objectstorage_preauthrequest.this.access_uri
 }
+
+output "cf_state_par" {
+  description = "State file PAR URI for cf-infra"
+  value = oci_objectstorage_preauthrequest.cf_infra.access_uri
+}
