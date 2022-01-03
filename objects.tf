71d8dad67505e439b212b5bd801be52d4ae32abd
lexffe <lexffe@users.noreply.github.com>
2022-01-03 18:56:50 +0000

    add second state file

--- objects.tf
+++ objects.tf
@@ -25,3 +25,30 @@ resource "oci_objectstorage_preauthrequest" "cf_infra_state" {
   namespace    = data.oci_objectstorage_namespace.ns.namespace
   time_expires = "2022-06-30T23:59:59.000Z"
 }
+
+resource "oci_objectstorage_object" "cf_stuff_state" {
+  bucket    = oci_objectstorage_bucket.state.name
+  namespace = data.oci_objectstorage_namespace.ns.namespace
+  object    = "cf-infra.tfstate"
+
+  content = "" # content managed by external world
+}
+
+resource "oci_objectstorage_preauthrequest" "cf_stuff_state" {
+  object_name  = oci_objectstorage_object.cf_infra_state.object
+  access_type  = "ObjectReadWrite"
+  bucket       = oci_objectstorage_bucket.state.name
+  name         = "bootstrap"
+  namespace    = data.oci_objectstorage_namespace.ns.namespace
+  time_expires = "2022-06-30T23:59:59.000Z"
+}
+
+# Remote state access
+resource "oci_objectstorage_preauthrequest" "cf_infra_remote_state" {
+  object_name  = oci_objectstorage_object.cf_infra_state.object
+  access_type  = "ObjectReadWrite"
+  bucket       = oci_objectstorage_bucket.state.name
+  name         = "bootstrap"
+  namespace    = data.oci_objectstorage_namespace.ns.namespace
+  time_expires = "2022-06-30T23:59:59.000Z"
+}
